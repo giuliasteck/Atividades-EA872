0adc0966cf1b6606ba587e89e382c067e4ef8731
giuliasteck <giuliasteck@gmail.com>
2020-10-12 18:16:15 -0300

    Funções adicionadas no Construtor da View

--- Atividade-2/view.cpp
+++ Atividade-2/view.cpp
@@ -16,27 +16,3 @@ const int View::getLargura() {return largura;}
 
 void View::imprimirCoordenadas() {std::cout << '(' << X << ',' << Y << ')' << std::endl;}
 
-void View::CriarJanela(SDL_Window *window){
-	window = nullptr;
-	window = SDL_CreateWindow ("Demonstração do SDL2",
-			SDL_WINDOWPOS_UNDEFINED,
-			SDL_WINDOWPOS_UNDEFINED,
-			largura,
-			altura,
-			SDL_WINDOW_SHOWN);
-	if (window==nullptr) { // Em caso de erro...
-    		std::cout << SDL_GetError();
-    		SDL_Quit();
-   	}
-}
-
-void Renderizar(SDL_Window *window, SDL_Renderer *renderer){
-      renderer = SDL_CreateRenderer(
-      window, -1,
-      SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-  	if (renderer==nullptr) { // Em caso de erro...
-    		SDL_DestroyWindow(window);
-    		std::cout << SDL_GetError();
-		SDL_Quit();
-  }
-}
--- Atividade-2/view.hpp
+++ Atividade-2/view.hpp
@@ -3,13 +3,32 @@ class View{
                 const int altura = 400;
                 const int largura = 200;
                 int X, Y;
-		SDL_Window *window = nullptr;
-		SDL_Renderer *renderer = nullptr;
         public:
                 View(){
                         X = 0;
                         Y = 0;
-                }
+
+		    	SDL_Window *window = nullptr;
+        		window = SDL_CreateWindow ("Demonstração do SDL2",
+                        	SDL_WINDOWPOS_UNDEFINED,
+                        	SDL_WINDOWPOS_UNDEFINED,
+                        	largura,
+                        	altura,
+                        	SDL_WINDOW_SHOWN);
+        		if (window==nullptr) { // Em caso de erro...
+                		std::cout << SDL_GetError();
+                		SDL_Quit();
+        		}
+			
+
+			SDL_Renderer* renderer = SDL_CreateRenderer(window, -1,
+     				SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+        		if (renderer==nullptr) { // Em caso de erro...
+                		SDL_DestroyWindow(window);
+                		std::cout << SDL_GetError();
+                		SDL_Quit();
+  			}
+		}
 
                 void setX(int x);
                 int getX();
@@ -18,9 +37,6 @@ class View{
 
                 const int getAltura();
                 const int getLargura();
-
-		void CriarJanela(SDL_Window *window);
-		void Renderizar(SDL_Window *window, SDL_Renderer *renderer);
 
                 void imprimirCoordenadas();
 };
